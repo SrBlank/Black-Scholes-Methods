6a87ec824918fb106c81e003e215dde71a534aba
SrBlank <rafiqsaad500@gmail.com>
2023-04-01 12:04:55 -0500

    added main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# Authors: Saad Rafiq and Josh Avery
+# Numerical Analysis II
+# Desc: Implementation of the bionomial tree, crank-nicolson, and
+#   runge kutta method on the black scholes equation
+
