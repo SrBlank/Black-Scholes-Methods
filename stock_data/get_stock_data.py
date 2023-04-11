30a981e1d0e317d716fabad29aa2b328cd51f7f0
SrBlank <rafiqsaad500@gmail.com>
2023-04-11 17:36:50 -0500

    added google stock data

--- stock_data/get_stock_data.py
+++ stock_data/get_stock_data.py
@@ -2,11 +2,11 @@ import yfinance as yf
 import pandas as pd
 
 # Get the Apple stock data
-aapl = yf.Ticker("AAPL")
+aapl = yf.Ticker("GOOGL")
 
 # Get the historical price data for the last 252 trading days
 hist = aapl.history(period="1y")
 hist = hist.tail(252)
 
 # Save the data to a CSV file
-hist.to_csv("aapl_stock_data.csv")
+hist.to_csv("googl_stock_data.csv")
